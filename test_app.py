7603de6812dbf01eaeeeea5dddd4b338d3871471
Vijay Srinivas Tida <vijaysrinivas.tida@gmail.com>
2026-06-21 10:41:43 -0500

    added code for sub

--- test_app.py
+++ test_app.py
@@ -6,8 +6,14 @@ def test_add():
     assert add(0,0) == 0
     assert add(-1,-1) == -2
 
+def test_subtract():
+    from app import subtract
+    assert subtract(5,2) == 3
+    assert subtract(1,-1) == 2
+    assert subtract(0,0) == 0
+    assert subtract(-1,-1) == 0
 # add main function to run the tests
 if __name__ == "__main__":
     test_add()
+    test_subtract()
     print("All tests passed!")
-    
